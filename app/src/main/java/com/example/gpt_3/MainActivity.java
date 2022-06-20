783831ac16d3b731928f19b40f3c5340da3a9220
NotInteger <qkrwjtn1236@gmail.com>
2022-06-20 21:11:37 +0900

    http connect

--- app/src/main/java/com/example/gpt_3/MainActivity.java
+++ app/src/main/java/com/example/gpt_3/MainActivity.java
@@ -3,10 +3,20 @@ package com.example.gpt_3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -15,13 +25,17 @@ import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     Button button;
     EditText editText;
     TextView textView;
 
-    private String str,receiveMsg;
+    // private String str,receiveMsg;
+
+    static RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,39 +47,37 @@ public class MainActivity extends AppCompatActivity {
         editText = findViewById(R.id.text);
         textView = findViewById(R.id.output);
 
+        String url = "http://175.207.128.71:5000/?a="+editText.getText().toString();
+//        String url = "https://www.naver.com/";
         button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-
-                String url = "http://192.168.43.95:5000/?a=";
-                String name = editText.getText().toString();
-                url = url+name;
-
-                try {
-                    URL targetURL = new URL(url);
-
-                    HttpURLConnection connection = (HttpURLConnection) targetURL.openConnection();
-                    if (connection.getResponseCode() == connection.HTTP_OK) {
-                        InputStreamReader tmp = new InputStreamReader(connection.getInputStream(), "UTF-8");
-                        BufferedReader reader = new BufferedReader(tmp);
-                        StringBuffer buffer = new StringBuffer();
-                        while ((str = reader.readLine()) != null) {
-                            buffer.append(str);
-                        }
-                        receiveMsg = buffer.toString();
-
-                        textView.setText(receiveMsg);
-                        reader.close();
-                }
-
-            } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                } catch (UnsupportedEncodingException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+            public void onClick(View v) {
+                httpRequest(url);
+                Toast.makeText(MainActivity.this,url,Toast.LENGTH_SHORT).show();
             }
         });
+
+
+        if (requestQueue == null){
+            requestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+    }
+
+    public void httpRequest(String url){
+        StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                textView.setText(response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.i("errorResponse", String.valueOf(error));
+            }
+        }
+        );
+
+        request.setShouldCache(false);
+        requestQueue.add(request);
     }
 }
